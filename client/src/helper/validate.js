84fd898ffadfbbcd2bf74deeac6ad0b577d1fea8
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-24 23:19:15 +0530

    made password component ui and used formik to validate password

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -6,6 +6,12 @@ export async function usernameValidate(values) {
   return errors;
 }
 
+/* Validate Login Page Password */
+export async function passwordValidate(values) {
+  const errors = passwordVerify({}, values);
+  return errors;
+}
+
 /* Validate Username */
 function usernameVerify(error = {}, values) {
   if (!values.username) {
@@ -14,4 +20,29 @@ function usernameVerify(error = {}, values) {
     error.username = toast.error("Invalid Username.....!");
   }
   return error;
+}
+
+/* Validate Password */
+function passwordVerify(errors = {}, values) {
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  const upperCaseChars = /[A-Z]/;
+
+  if (!values.password) {
+    errors.password = toast.error("Password Required.....!");
+  } else if (values.password.includes(" ")) {
+    errors.password = toast.error("Invalid Password.....!");
+  } else if (values.password.length < 4) {
+    errors.password = toast.error(
+      "Password must have atleast 4 characters.....!"
+    );
+  } else if (!specialChars.test(values.password)) {
+    errors.password = toast.error(
+      "Password must have special characters.....!"
+    );
+  } else if (!upperCaseChars.test(values.password)) {
+    errors.password = toast.error(
+      "Password must have an uppercase letter.....!"
+    );
+  }
+  return errors;
 }
