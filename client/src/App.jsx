1fdebd091aaa1cd4a52300a96605cf0022540574
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-24 20:51:57 +0530

    added tailwind css to project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 function App() {
   return (
     <main>
-      <h1>Hello World!</h1>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
     </main>
   );
 }
