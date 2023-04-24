6075118080d053834af77240580412c73a204385
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-24 23:01:20 +0530

    created form validation using formik for username

--- client/src/components/UserName.jsx
+++ client/src/components/UserName.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import avatar from "../assets/profile.png";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { usernameValidate } from "../helper/validate";
 
 // Import CSS
 import styles from "../styles/UserName.module.css";
 
 export default function UserName() {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+    },
+    validate: usernameValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false} />
+
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
@@ -17,13 +34,14 @@ export default function UserName() {
             </span>
           </div>
 
-          <form className="py-1">
+          <form className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
               <img src={avatar} className={styles.profile_img} alt="avatar" />
             </div>
 
             <div className="textbox flex flex-col items-center gap-6">
               <input
+                {...formik.getFieldProps("username")}
                 className={styles.textbox}
                 type="text"
                 placeholder="Username"
