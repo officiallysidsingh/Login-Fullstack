4b92b6481e5b10dc4b1bff58bdd79acddd1ff7f6
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-24 21:10:54 +0530

    added routes for each page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Import Components
+import UserName from "./components/UserName";
+import Password from "./components/Password";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
+
+// Root Routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <UserName />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
+
 function App() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <RouterProvider router={router}></RouterProvider>
     </main>
   );
 }
