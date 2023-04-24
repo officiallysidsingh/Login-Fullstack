10e29204da17df6d6e1a0e277e747cdd021e53e8
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-24 23:28:59 +0530

    made the ui of recovery component

--- client/src/components/Password.jsx
+++ client/src/components/Password.jsx
@@ -28,7 +28,7 @@ export default function Password() {
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold">Hello Again</h4>
+            <h4 className="text-5xl font-bold">Hello Again!</h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-500">
               Explore More by connecting with us
             </span>
