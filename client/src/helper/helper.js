6a5e9a90c71d7b4fb6ea49717266951a5dd9cbc5
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-29 21:53:31 +0530

    reisterUser created successfully

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -13,11 +13,36 @@ export const authenticate = async (username) => {
 };
 
 /* Get User Details */
-export const getUser = async (username) => {
+export const getUser = async ({ username }) => {
   try {
-    const res = await axios.get(`/api/user/${username}`);
-    return res.data;
+    const { data } = await axios.get(`/api/user/${username}`);
+    return { data };
   } catch (error) {
     return { error: "Password doesn't match!" };
+  }
+};
+
+/* Register User Function */
+
+export const registerUser = async (credentials) => {
+  try {
+    // if any error remove data: {message} and use data directly
+    const {
+      data: { message },
+      status,
+    } = await axios.post("/api/register", credentials);
+    let { username, email } = credentials;
+
+    // Send Email
+    if (status === 201) {
+      await axios.post("/api/registerMail", {
+        username,
+        userEmail: email,
+        text: message,
+      });
+    }
+    return Promise.resolve(message);
+  } catch (error) {
+    return Promise.reject({ error });
   }
 };
