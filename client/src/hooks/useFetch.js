06fe9ca77b663bb01b723823ea2fcfbe27893b7c
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-30 11:11:16 +0530

    made custom hook to fetch data

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+axios.defaults.baseURL = import.meta.env.VITE_APP_SERVER_URI;
+
+export default function useFetch(query) {
+  const [getData, setData] = useState({
+    isLoading: false,
+    apiData: undefined,
+    status: null,
+    serverError: null,
+  });
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchData = async () => {
+      try {
+        setData((prev) => ({ ...prev, isLoading: true }));
+
+        const { data, status } = await axios.get(`/api/${query}`);
+
+        if (status === 200) {
+          setData((prev) => ({ ...prev, isLoading: false }));
+          setData((prev) => ({ ...prev, apiData: data, status: status }));
+        }
+
+        setData((prev) => ({ ...prev, isLoading: false }));
+      } catch (error) {
+        setData((prev) => ({ ...prev, isLoading: false, serverError: error }));
+      }
+    };
+
+    fetchData();
+  }, [query]);
+
+  return [getData, setData];
+}
