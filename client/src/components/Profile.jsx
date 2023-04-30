cbf64fe67e6b044a141bcf1e847acacf96855620
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-30 20:20:14 +0530

    made update profile page dynamic and linked to db

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import avatar from "../assets/profile.png";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import { profileValidate } from "../helper/validate";
 import convertToBase64 from "../helper/convert";
+import useFetch from "../hooks/useFetch";
+import { useAuthStore } from "../store/store";
+import { updateUser } from "../helper/helper";
 
 // Import CSS
 import styles from "../styles/UserName.module.css";
@@ -13,20 +16,32 @@ import extend from "../styles/Profile.module.css";
 export default function Profile() {
   const [file, setFile] = useState();
 
+  const { username } = useAuthStore((state) => state.auth);
+  const [{ isLoading, apiData, serverError }] = useFetch(`/user/${username}`);
+
   const formik = useFormik({
     initialValues: {
-      firstName: "",
-      lastName: "",
-      email: "demoemail@gmail.com",
-      mobile: "",
-      address: "",
+      firstName: apiData?.firstName || "",
+      lastName: apiData?.lastName || "",
+      email: apiData?.email || "",
+      mobile: apiData?.mobile || "",
+      address: apiData?.address || "",
     },
+    enableReinitialize: true,
     validate: profileValidate,
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
-      values = await Object.assign(values, { profile: file || "" });
-      console.log(values);
+      values = await Object.assign(values, {
+        profile: file || apiData?.profile || "",
+      });
+      let updatePromise = updateUser(values);
+
+      toast.promise(updatePromise, {
+        loading: "Updating...",
+        success: <b>Updated Successfully...!</b>,
+        error: <b>Failed to Update...!</b>,
+      });
     },
   });
 
@@ -35,6 +50,14 @@ export default function Profile() {
     const base64 = await convertToBase64(e.target.files[0]);
     setFile(base64);
   };
+
+  if (isLoading) {
+    return <h1 className="text-2xl font-bold">Loading...</h1>;
+  }
+
+  if (serverError) {
+    return <h1 className="text-xl text-red-500">{serverError.message}</h1>;
+  }
 
   return (
     <div className="container mx-auto">
@@ -56,7 +79,7 @@ export default function Profile() {
             <div className="profile flex justify-center py-4">
               <label htmlFor="profile">
                 <img
-                  src={file || avatar}
+                  src={apiData?.profile || file || avatar}
                   className={`${styles.profile_img} ${extend.profile_img}`}
                   alt="avatar"
                 />
